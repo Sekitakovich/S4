7a67fbd3fe56e13d7ef78dfe517f7ac6ace1ac11
K.Seki <k-seki@klabo.co.jp>
2020-12-20 18:28:23 +0900

    3rd commit

--- sample.py
+++ sample.py
@@ -1,67 +1,68 @@
+from typing import List
 from janome.tokenizer import Tokenizer
 from dataclasses import dataclass
 from loguru import logger
 
 
 @dataclass()
-class Parts(object):
+class Word(object):
     text: str
     isNoun: bool
+    parts: int
 
 
 class Sample(object):
     def __init__(self):
         self.tokenizer = Tokenizer()
+        self.hits = ['名詞', '接頭詞', '接尾詞', '形容詞']
 
-    def exec(self, *, src: str):
-        listNoun = ['名詞', '接頭詞', '接尾詞', '形容詞']
-        buffer = []
+    def analyze(self, *, src: str) -> List[Word]:
         result = []
-
-        def append():
-            length = len(buffer)
-            if length:
-                if length>1:
-                    logger.info(f'join [{buffer}]')
-                ppp = ''.join(buffer)
-                buffer.clear()
-                result.append(Parts(text=ppp, isNoun=True))
-
+        lastNoun = True
+        buffer = []
         for token in self.tokenizer.tokenize(src):
+            thisNoun = False
             text = token.node.surface
-            logger.debug(token)
             if token.extra:
-                ooo = token.extra[0].split(',')
-                noun = ooo[0] in listNoun
+                extra = token.extra
+                ooo = extra[0].split(',')
+                thisNoun = ooo[0] in self.hits
             else:
-                # noun = False
-                noun = True
+                thisNoun = True
 
-            if noun:
+            if thisNoun:
+                if lastNoun is False:
+                    if len(buffer):
+                        word = ''.join(buffer)
+                        result.append(Word(text=word, isNoun=False, parts=len(buffer)))
+                        buffer.clear()
                 buffer.append(text)
-                pass
+
             else:
-                append()
-                result.append(Parts(text=text, isNoun=False))
+                if lastNoun is True:
+                    if len(buffer):
+                        word = ''.join(buffer)
+                        result.append(Word(text=word, isNoun=True, parts=len(buffer)))
+                        buffer.clear()
+                buffer.append(text)
 
-        append()
+            lastNoun = thisNoun
 
-        for p in result:
-            if p.isNoun:
-                logger.debug(p.text)
-            else:
-                logger.info(p.text)
+        if len(buffer):
+            word = ''.join(buffer)
+            result.append(Word(text=word, isNoun=lastNoun, parts=len(buffer)))
 
+        return (result)
 
 if __name__ == '__main__':
 
     def main():
         sample = Sample()
 
-        src = '2020年の国民的大事件は「新型コロナウイルス」と「アンジャッシュ渡部不倫騒動」と「ホンダF1撤退発表」'
-        src = '今年1月に上演された舞台『鬼滅の刃』の新作公演が、2021年夏に上演されることが決定した。19日にオンライン上で開催された「ジャンプフェスタ 2021 ONLINE 」にて発表された。'
-        src = '電源構成は、特定の電源や燃料源への依存度が過度に高まらないようにしつつ、低廉で安定的なベースロード電源を国際的にも遜色のない水準で確保すること、安定供給に必要な予備力、調整力を堅持すること、環境への適合を図ることが重要であり、バランスのとれた電源構成の実現に注力していく必要がある。一方、東京電力福島第一原子力発電所事故後、電力需要に変化が見られるようになっている。こうした需要動向の変化を踏まえつつ、節電や、空調エネルギーのピークカットなどピーク対策の取組を進めることで電力の負荷平準化を図り、供給構造の効率化を進めていくことが必要である。'
-        sample.exec(src=src)
+        src = '東京電力福島第一原子力発電所には、東日本大震災によるメルトダウンから9年が過ぎた今でも大量の放射性廃棄物が放置されている'
+        result = sample.analyze(src=src)
 
+        for w in result:
+            logger.info(w)
 
     main()
